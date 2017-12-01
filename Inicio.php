d0ec2cf52ce4a9476f6cff996cd1210ebc536ad1
gabrielbaron16 <32284026+gabrielbaron16@users.noreply.github.com>
2017-12-01 18:22:44 -0400

    Add files via upload

--- Inicio.php
+++ Inicio.php
@@ -81,7 +81,7 @@ ob_start();
   </tr>
 
   <tr>
-  <td colspan="2" align="center"><input name="olvidecontra" type="submit" class="boton" value="Olvide mi contraseña"/> <td>
+  <td colspan="2" align="center"><input name="olvidecontra" type="submit" class="boton" value="Olvide mi contraseña"  formnovalidate/> <td>
   </tr> 
      </form>
   
@@ -154,17 +154,14 @@ else if (isset($_POST["olvidecontra"])){ // si presionas olvide mi contrasena, t
           curl_close($ch);
           return($codigo);
         }
-
  include ("Usuario.php");
  $nombre=' ';
  $clave=' ';
  $direccion=' ';
  
-
  if (isset($_POST["enviando"])) {
   $nombre=$_POST["nombre_usuario"];
   $clave=$_POST["contrasena_usuario"];
-
   $codigo=TransformToJson($nombre,$clave);
   if ($codigo==404){
   ?>
@@ -203,8 +200,7 @@ else if (isset($_POST["olvidecontra"])){ // si presionas olvide mi contrasena, t
   }
  }
 ?>
-  
-  
+
 <footer>
 </footer>
 </body>
